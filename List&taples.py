08320125b0fbbef478be60fdf1d39646cc4491f0
Zain Ul Aabdin <133965930+tangsan-Haotian@users.noreply.github.com>
2026-02-10 08:45:34 +0000

    these are new changes i did regarding  lists

---
+++ List&taples.py
@@ -0,0 +1,11 @@
+names=['abbas','zain','azeem','kamran','noor']
+names.append("ali")
+print(names)
+"to sort the list in accending order "
+print(names.sort())
+print(names)
+"to print the list in reverse"
+print(names.sort(reverse=True))
+print(names)
+"to print list type"
+print(type(names))
---
+++ Practice.py
@@ -0,0 +1,15 @@
+print("kindly enter the agr to print your age calculator ")
+age=int(input("enter age here"))
+print(age,"\n")
+print(type(age))
+
+''' this id the list and otehr practice to help me to jet my job and answer that that thing '''
+name =["zain","Ali","Pranjal","Sarah"]
+print(name)
+print(type(name))
+'adding data in list performing mutation in list  '
+marks=[22.4,22.4,33.5,19.8,10.1]
+print(marks)
+marks.append(89.77)
+
+print(marks)

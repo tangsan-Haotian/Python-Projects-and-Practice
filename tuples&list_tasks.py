79098558e28bf8e2e5039b307bb558b134abba92
Zain Ul Aabdin <133965930+tangsan-Haotian@users.noreply.github.com>
2026-02-11 14:10:21 +0000

    this file talks about the dictionary and other important topics

---
+++ tuples&list_tasks.py
@@ -0,0 +1,15 @@
+movies=[]
+print("kindly print input the main task to perfomed")
+m1=input("input the first movie ")
+m2=input("input the second movie ")
+m3=input("input the 3rd movie ")
+m4=input("input the 4rth movie ")
+m5=input("input the 5tht movie ")
+
+movies.append(m1)
+movies.append(m2)
+movies.append(m3)
+movies.append(m4)
+movies.append(m5)
+
+print(movies)

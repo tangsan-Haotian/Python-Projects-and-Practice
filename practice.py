c683d931239a137d2cf6f7edf4a6fd51c2c2da4f
Zain Ul Aabdin <133965930+tangsan-Haotian@users.noreply.github.com>
2026-02-11 07:01:40 -0800

    Add practice.py to demonstrate dictionary usage

---
+++ practice.py
@@ -0,0 +1,11 @@
+"this code is to understand dictionary "
+details = {
+
+"name": "zain",
+"school" :"Sukkur IBA",
+"class":"Last Year could be ",
+"age": "eligible to your dad",
+"number": "ask your mom"
+
+}
+print(details)

cd98b7fd8abe2b7c1418fada031589e2020fdc3f
Zain Ul Aabdin <133965930+tangsan-Haotian@users.noreply.github.com>
2026-02-12 18:55:45 +0000

    this is relatec the sets also

--- Dictionary.py
+++ Dictionary.py
@@ -1,11 +1,27 @@
-"this code is to understand dictionary "
-details = {
+#this code is to understand dictionary "
+# details = {
 
-"name": "zain",
-"school" :"Sukkur IBA",
-"class":"Last Year could be ",
-"age": "eligible to your dad",
-"number": "ask your mom"
+# "name": "zain",
+# "school" :"Sukkur IBA",
+# "class":"Last Year could be ",
+# "age": "eligible to your dad",
+# "number": "ask your mom"
 
-}
-print(details)
+# }
+#print(details)
+#this shows  only keys  and and feild values"
+#print(details.keys())
+
+#this shows thw basic values that blongs to feilds"
+# print(list(details.values()))
+
+
+#this blongs to sets i mean this code.
+collection = set()
+collection.add("zain")
+collection.add(30)
+collection.add((2,3,4,5,6,"pranjal"))
+#collection.add([2,3,4,1])   # this line could create error
+print(collection)
+# to check the length and elements in set
+print(len(collection))

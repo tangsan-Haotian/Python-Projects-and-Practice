a090260e850c117a34117e30415f53118f1b6c91
Zain Ul Aabdin <133965930+tangsan-Haotian@users.noreply.github.com>
2026-02-17 18:35:41 +0000

    this leads to understand the basics regarding indentaton and how python works

--- Loops.py
+++ Loops.py
@@ -22,3 +22,16 @@
 #     count -= 1
 
 
+#we can use list tupples and oters like arrays in other languages 
+num = (1,2,4,5,5.6,6,7.4,3)
+
+
+#this was to search some main things in this and not giving any objection or some thing else 
+# i = 0
+# while i < len(num):
+#     print(num[i])   # square brackets
+#     i += 1          # next line me
+
+
+#to found the person or the name that how many times it is in the list or tupple 
+name =( "Noor" ,"raja")
---
+++ loop.py
@@ -0,0 +1,20 @@
+#this is to practice in efficient way and make many problems tolvable
+
+# while i<10 :
+#     if(i>7):
+#         break
+#     else:
+#         print(i)
+#         i+=1
+#this was to create and use while loop and break statements 
+
+i=1
+while(i<20):
+    if(i%2==0):
+        print(i,"is even")
+        i+=1
+
+        
+    else:
+        print(i," is odd")
+        i+=1

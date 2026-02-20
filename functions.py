6358bfc5e0e77b6c53a424e688aef2437fe616b0
Zain Ul Aabdin <133965930+tangsan-Haotian@users.noreply.github.com>
2026-02-20 17:39:14 +0000

    this is mostly explaing about seperator and enf function that this will helps to print things over same line

--- functions.py
+++ functions.py
@@ -9,3 +9,13 @@
 # num2=int(input("firstnumber"))
 # print(fun(num1,num2))
 
+
+#using seperator and other thinsgs to understan this as its full usage in PDF and UDF
+#this seperator show a extra spacce in my own things
+print("iam on and here to help you ", end=" ")
+
+#usinf ending perapeter and its usage to understand it in python and then other things to undertstand it in deeper things 
+print("iam here ti understand you love",)
+
+#both values will be printed over same line 
+

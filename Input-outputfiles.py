7252a2bd86ee5111c0b727d593e892d68c9c953d
Zain Ul Aabdin <133965930+tangsan-Haotian@users.noreply.github.com>
2026-02-25 18:25:36 +0000

    thiss was to learn the basics and formal thisngs regarding the system and other perameters

--- Input-outputfiles.py
+++ Input-outputfiles.py
@@ -1,1 +1,6 @@
 #this file is  being used  to understand file handling in python
+
+f=open("sample.txt","r")  #if we didnt set the mod then Sytem will asume that its in read mode
+data=f.read()
+print(data)
+print(type(data)) # this will only functionale if we sre using it in system not online 

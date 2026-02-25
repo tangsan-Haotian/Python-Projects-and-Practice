2ffa1ffadb89e14662ae16a8572110742222a69f
Zain Ul Aabdin <133965930+tangsan-Haotian@users.noreply.github.com>
2026-02-25 18:50:32 +0000

    this also leads the second code means that it is for direct closing no need to close and sut it manually

--- Input-outputfiles.py
+++ Input-outputfiles.py
@@ -1,6 +1,23 @@
 #this file is  being used  to understand file handling in python
 
-f=open("sample.txt","r")  #if we didnt set the mod then Sytem will asume that its in read mode
-data=f.read()
-print(data)
-print(type(data)) # this will only functionale if we sre using it in system not online 
+# f=open("sample.txt","r")  #if we didnt set the mod then Sytem will asume that its in read mode
+# data=f.read()
+# print(data)
+# print(type(data)) # this will only functionale if we sre using it in 
+# 
+# 
+# system not online 
+
+
+#this is the manual method to open and close the file in proper way 
+file= open("Dictionary.py", "r") 
+content = file.read()
+print(content)
+file.close()
+
+
+#this automaticly close file and then suggest the best output regarding it 
+
+with open("functions.py", "r") as file:
+    content = file.read()
+    print(content)

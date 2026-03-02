91503eea2207010d33dfaf0cd695c2ba5b13ce52
Zain Ul Aabdin <133965930+tangsan-Haotian@users.noreply.github.com>
2026-03-02 06:41:03 +0000

    this was the object and class relation related to undertsant the basic of its work

--- OOP_classes.py
+++ OOP_classes.py
@@ -1,1 +1,16 @@
 #this was being created to understand the basics of oops
+class student:
+    name="zain"
+    Cast="solangi"
+    number=3078232501
+
+# print(student.name)
+#this a blue [print type class that emplifies the details that shows that how the things have properties and what properties have they ]
+
+#this is object by using class attributes by using object we are calling propertises ao classes 
+s1=student()
+print(s1.name)
+
+s2=student()
+s2.name="Oshaque"
+print()

c9564c8b519ac2401ec64a8ac6a74c3ba69f3e0f
Zain Ul Aabdin <133965930+tangsan-Haotian@users.noreply.github.com>
2026-03-10 19:19:14 +0000

    this was to unverstand the constructor and the methods how theu work and poerform taks

--- OOP_classes.py
+++ OOP_classes.py
@@ -8,11 +8,28 @@ class student:
 #this a blue [print type class that emplifies the details that shows that how the things have properties and what properties have they ]
 
 #this is object by using class attributes by using object we are calling propertises ao classes 
-s1=student()
-print(s1.name)
+# s1=student()
+# print(s1.name)
 
-s2=student()
-s2.name="Oshaque"
-print(s2.name)
-print(s1.name)
+# s2=student()
+# s2.name="Oshaque"
+# print(s2.name)
+# print(s1.name)
 
+class student:
+    name="shazain"
+    age="22"
+    school="sukkur IBA Public"
+    ain="be like Gojo"
+
+#this is constrctor which has been called every time 
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+        print("this is the data for new student")
+
+
+s1=student("karan","79")
+print(s1.name,s1.marks)
+print(s1.marks)
+        

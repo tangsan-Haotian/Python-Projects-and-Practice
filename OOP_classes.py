426b595892f4cf2b9478dca2fb59df33cdad1eec
Zain Ul Aabdin <133965930+tangsan-Haotian@users.noreply.github.com>
2026-03-12 18:25:02 +0000

    this file is seconf part of opps in pythoin

--- OOP_classes.py
+++ OOP_classes.py
@@ -16,36 +16,40 @@ class student:
 # print(s2.name)
 # print(s1.name)
 
-class student:
-    name = "shazain"
-    age = "22"
-    school = "sukkur IBA Public"
-    ain = "be like Gojo"
+# class student:
+#     name = "shazain"
+#     age = "22"
+#     school = "sukkur IBA Public"
+#     ain = "be like Gojo"
 
-    # constructor is set to be denote unique elements of the data base in mython and others to understan the things 
-    def __init__(self, name, marks,age,previous_school):
-        self.name = name
-        self.marks = marks
-        self.age=age
-        self.previous_school=previous_school
+#     # constructor is set to be denote unique elements of the data base in mython and others to understan the things 
+#     def __init__(self, name, marks,age,previous_school):
+#         self.name = name
+#         self.marks = marks
+#         self.age=age
+#         self.previous_school=previous_school
 
-        print("this is the data for new student")
+#         print("this is the data for new student")
 
-    def wellcome(self):
-        print("welcome students this is the world that you always ask for")
+#     def wellcome(self):
+#         print("welcome students this is the world that you always ask for")
 
-s1 = student("karan", "79",60,"Queen  mery")
+# s1 = student("karan", "79",60,"Queen  mery")
 
-s2=student("atif",99,22,"AL Ahad Public school")
+# s2=student("atif",99,22,"AL Ahad Public school")
 
 
-s1.wellcome()
-#this shows how to use these things and understand them peroperly in sequesnces 
+# s1.wellcome()
+# #this shows how to use these things and understand them peroperly in sequesnces 
 
-#this was to make it correct
+# #this was to make it correct
 
-print(s1.name , s1.previous_school,s1.school)
-#this shows how the things are related and deffrant this helps to save memory 
-print(s2.name , s2.previous_school,s2.school)
+# print(s1.name , s1.previous_school,s1.school)
+# #this shows how the things are related and deffrant this helps to save memory 
+# print(s2.name , s2.previous_school,s2.school)
 
+class Account:
+    def__init__(self,balance,account):
+    self.balance= balance
+    self.account_no= account
 
---
+++ oops.py
@@ -0,0 +1,1 @@
+#this file is being used to understand the basics and advance concepts of the opps in python 

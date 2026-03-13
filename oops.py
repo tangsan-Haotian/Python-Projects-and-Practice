e657446e9f741c9610aa9d6ec2603330ea85cf76
Zain Ul Aabdin <133965930+tangsan-Haotian@users.noreply.github.com>
2026-03-13 10:14:09 +0000

    this was about secont part and explain the working of delete keyword

--- oops.py
+++ oops.py
@@ -1,1 +1,19 @@
-#this file is being used to understand the basics and advance concepts of the opps in python 
+#this file is being used to understand the basics and advance concepts of the opps in
+# class Account:
+#     def __init__(self,acc_no,acc_pass):
+#         self.acc_no= acc_no
+#         self.acc_pass=acc_pass
+#         pass
+
+class Student:
+    def __init__(self,name):
+        self.name=name
+
+s1=Student("Abbas")
+
+s2=Student("Asad")
+print(s1.name)
+print(s2.name)
+del s2.name
+
+print(s2.name)
